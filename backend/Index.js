28c49bff143098631f228344f5431c04815869d4
talismo.admin <seamoss@talismo.io>
2022-12-14 11:52:01 -0800

    added the .env file

--- backend/Index.js
+++ backend/Index.js
@@ -14,7 +14,7 @@ class Index {
     static setUpServer() {
         Index.app.use(cors());
         Index.app.use(express.json());
-        Index.app.use('/api/v1/movies', MoviesRoute.configRoutes(Index.router));
+        Index.app.use('/api/v0/tgates', MoviesRoute.configRoutes(Index.router));  // Movies === gate objects
         Index.app.use('*', (req, res) => {
             res.status(404).json({ error: 'not found' });
         });
@@ -22,3 +22,13 @@ class Index {
 }
 
 Index.main();
+
+// Public Gate Browsing
+/* Potential primary taxonomy set is gate objects, to be followed along the guide as synonymous with movie data.
+Filter aspects:
+- executable (calling from web app domain or its configured domain, active, and matching proven ownership)
+- created (mapped to the account of the current user as the creator of the gate)
+- by user profile attributes, as AND logic (Twitter handle)
+-- a public user profile can load the gates created by him
+-- ideally filtering is performed through URL parameters so that direct URLs can be shared
+*/
